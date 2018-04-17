bf8e37bd8aadcdc7eec838bfc5333224e229da55
DESKTOP-IK4MUI3\nj <87179471@qq.com>
2018-04-17 17:47:52 +0800

    修正图片上传路径问题

--- routers/upload.js
+++ routers/upload.js
@@ -1,16 +1,17 @@
 var express = require('express')
 var multer = require('multer')
+var path = require('path')
 var fs = require('fs')
 
 var router = express.Router()
 
-var dir = './public/upload/'
+var dir = path.resolve(__dirname, '../public/upload')
 var upload = multer({ dest: dir })
 
 router.post('/', upload.any(), function(req, res, next) {
 
   //console.log(req.files[0])  // 上传的文件信息
-  let _path = dir + req.files[0].filename
+  let _path = dir + '/' + req.files[0].filename
 
   fs.readFile( req.files[0].path, function (err, data) {
     fs.rename(_path, _path + '.jpg', function (err) {
